e8b79befa6a08c9202b2718bd5e1ee0ffd657a62
Ben McNulty <benmcnulty@live.com>
2018-11-29 12:27:08 -0500

    File clean up

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,15 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
-
-    void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
-
     // Use this for initialization
     void Start () {
         Invoke("LoadFirstScene", 2f);
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
