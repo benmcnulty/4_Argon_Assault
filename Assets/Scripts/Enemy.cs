b8598f86ba98160015eed5fa9380e89e4ed0e54f
Ben McNulty <benmcnulty@live.com>
2018-11-30 02:12:01 -0500

    Added scoreboard UI

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] GameObject crashFX;
+    [SerializeField] Transform parent;
+
+	// Use this for initialization
+	void Start ()
+    {
+        AddNonTriggerBoxCollider();
+	}
+	
+    void OnParticleCollision(GameObject other)
+    {
+        GameObject fx = Instantiate(crashFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+
+    void AddNonTriggerBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+}
