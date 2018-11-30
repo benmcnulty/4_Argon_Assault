635a26b5dc1fc526b9b55d49f2156a08b4b9a327
Ben McNulty <benmcnulty@live.com>
2018-11-30 03:32:02 -0500

    Commit Reverted

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,24 +7,17 @@ public class Enemy : MonoBehaviour {
 
     [SerializeField] GameObject crashFX;
     [SerializeField] Transform parent;
-    [SerializeField] int scorePerHit = 12;
-
-    ScoreBoard scoreBoard;
 
 	// Use this for initialization
 	void Start ()
     {
         AddNonTriggerBoxCollider();
-        scoreBoard = FindObjectOfType<ScoreBoard>();
 	}
 	
     void OnParticleCollision(GameObject other)
     {
-        scoreBoard.ScoreHit(scorePerHit);
-
         GameObject fx = Instantiate(crashFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
-
         Destroy(gameObject);
     }
 
